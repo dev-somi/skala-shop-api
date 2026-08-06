c11217c28d51ea5e1ebca7097134cd76bf39ff1d
Somi <somi.bak.dev@gmail.com>
2026-08-06 14:39:39 +0900

    feat: common폴더 Response 클래스 구현

--- src/main/java/com/sk/skala/shopapi/service/ProductService.java
+++ src/main/java/com/sk/skala/shopapi/service/ProductService.java
@@ -1,5 +1,13 @@
 package com.sk.skala.shopapi.service;
 
+import org.springframework.stereotype.Service;
+
+import com.sk.skala.shopapi.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
-    
+    private final ProductRepository productRepository;
 }
